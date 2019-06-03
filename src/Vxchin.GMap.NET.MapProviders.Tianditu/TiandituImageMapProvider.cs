0fdebf473ad322ce8737acfe091fedacf09bae6b
Vincent Chin <vxchin@outlook.com>
2019-06-03 15:30:37 +0800

    Add source code
    Signed-off-by: Vincent Chin <vxchin@outlook.com>

---
+++ src/Vxchin.GMap.NET.MapProviders.Tianditu/TiandituImageMapProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vxchin.GMap.NET.MapProviders.Tianditu
+{
+    public class TiandituImageMapProvider : TiandituMapProviderBase
+    {
+        private static TiandituImageMapProvider instance;
+
+        public static TiandituImageMapProvider Instance => instance ?? (instance = new TiandituImageMapProvider());
+
+        public override Guid Id => new Guid("1883D8B5-9279-4D13-934A-AE333F20B5A1");
+
+        public override string Name => nameof(TiandituImageMapProvider);
+
+        public TiandituImageMapProvider() : base("img") { }
+    }
+}
---
+++ src/Vxchin.GMap.NET.MapProviders.Tianditu/TiandituImageWithAnnotationMapProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GMap.NET.MapProviders;
+
+namespace Vxchin.GMap.NET.MapProviders.Tianditu
+{
+    public class TiandituImageWithAnnotationMapProvider : TiandituMapProviderBase
+    {
+        private static TiandituImageWithAnnotationMapProvider instance;
+
+        public static TiandituImageWithAnnotationMapProvider Instance => instance ?? (instance = new TiandituImageWithAnnotationMapProvider());
+
+
+        public override Guid Id => new Guid("802BE4D1-A1AD-4BF6-9C22-D2859529E38D");
+
+        public override string Name => nameof(TiandituImageWithAnnotationMapProvider);
+
+        public override GMapProvider[] Overlays => new GMapProvider[] { TiandituImageMapProvider.Instance, this };
+
+        public TiandituImageWithAnnotationMapProvider() : base("cia") { }
+    }
+}
